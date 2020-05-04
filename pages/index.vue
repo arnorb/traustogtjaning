4b25ab2a0d0a62a20d3413211b1204efce6dc6ba
Arnór Bogason <arnorbogason@Megabyte.local>
2020-05-04 15:14:58 +0000

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="container mx-auto px-4 mt-4">
+    <div>
+      <h1 class="sm:text-5xl text-2xl font-bold">
+        Traust og tjáning
+      </h1>
+      <h2 class="sm:text-3xl font-semibold mb-8">
+        Leiklistarnámskeið fyrir börn og fullorðna saman
+      </h2>
+
+      <div>
+
+        <div class="float-right ml-4 my-2 h32 border rounded w-1/3 p-6">
+          Námskeiðið er sex vikur og er kennt einu sinni í viku. Verð til 1. júlí er á sérstöku tilboði, aðeins <strong>29.990 krónur</strong>.
+
+          <button class="display bg-blue-600 text-white font-bold py-4 mt-4 rounded w-full">Skráning</button>
+        </div>
+
+        <p>Viltu rifja upp hvernig var að leika sér?</p>
+        <p>Viltu losna við stífni og hömlur og óttann við að koma fram?</p>
+        <p>Viltu skemmta þér með barninu þínu?</p>
+
+        <p>Ef svarið er já: Búum til leikrit!</p>
+
+        <p>Ég heiti Hrund Ólafsdóttir. Ég hef verið leiklistarkennari og íslensku- ensku og bókmenntakennari. Ég skrifa leikrit og leikstýri og svo hef ég leikið heilmikið.</p>
+
+        <p>Ég býð upp á leiklistartíma þar sem fyrstu skrefin eru tekin í gegnum netið til að byrja með en svo stefni ég að því að nemendur komi til mín í „raunheiminum“.  </p>
+
+        <p>Á átta til tíu klukkutíma námskeiði kenni ég börnum og fullorðnum að iðka leiklist saman og verða sjálfbjarga. Fullorðna fólkið kennir börnunum rammann; reglur, tími, skipulag, og börnin kenna þeim fullorðnu að leika sér, að skapa frá hjartanu, tjá sig. Ég kem með módelið til ykkar, kenni ykkur á það, skil það eftir og svo er hægt að koma <p>á framhaldsnámskeið að nokkrum vikum eða mánuðum liðnum.</p>
+
+        <p>Lærið að búa til leikrit.</p>
+        <p>Lærið að leika ykkur saman.</p>
+        <p>Lærið að skemmta ykkur og skapa í leiðinni.</p>
+        <p>Lærið að tala skýrar og miðla leiknum til áhorfenda.</p>
+
+        <p>Eftir námskeiðið hafið þið lært að:</p>
+
+        <p>Spinna út frá fyrirframgefnum aðstæðum.</p>
+        <p>Tala skýrar og miðla til áhorfenda.</p>
+        <p>Búa til leikþátt frá grunni og sýna.</p>
+
+        <p>Þú skráir þig á námskeið með eitt eða tvö börn, fimm ára og eldri, með þér.  Námskeiðið stendur í tvær til þrjár vikur, tvisvar  til þrisvar í viku. Ég legg inn efnið, fyrirkomulag og æfingar, í fyrsta tímanum, en þið verðið með mér á zoomi, teems eða jitsi, og þið æfið ykkur heima þar til næst. Þá legg ég inn fleiri æfingar og þið sýnið mér.  Ég læt ykkur búa til leikþátt og svo æfið þið ykkur að búa til leikþætti heima og þannig gengur þetta áfram.</p>
+
+        <p>Í lokin læt ég ykkur hafa skýrt módel sem byggir á fyrirkomulagi námskeiðsins og þá getið þið notað það hvenær sem er og jafnvel fengið fleiri í lið með ykkur.</p>
+
+        <blockqoute>
+          „Hugsum stórt en hlúum að því smáa.“
+ 
+​          <cite>Ken Robinson uppeldisgúru og leiklistarkennari</cite>
+
+        </blockqoute>
+      </div>
+
+      
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+p {
+  @apply mb-4;
+}
+</style>
